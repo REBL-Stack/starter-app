7abb2d05cd4986341c43fbb7317cc5da57d5ff0a
Terje Norderhaug <terje@in-progress.com>
2019-11-18 15:58:30 -0800

    Move Blockstack context provider to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useBlockstack} from 'react-blockstack'
+import { Blockstack } from 'react-blockstack/dist/context'
 import Profile from './Profile.js';
 import Landing from './Landing.js';
 
 export default function App (props) {
-  const { person } = useBlockstack()
+  const { person, signIn } = useBlockstack()
   return (
+  <Blockstack>
     <div className="site-wrapper">
       <div className="site-wrapper-inner">
-         {!person && <Landing />}
+         {signIn && <Landing />}
          {person && <Profile person={person} />}
       </div>
     </div>
+  </Blockstack>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App.js'
 import ReactBlockstack from 'react-blockstack'
-import { Blockstack } from 'react-blockstack/dist/context'
-import { AppConfig } from 'blockstack';
+import { AppConfig } from 'blockstack'
+import App from './App.js'
 
 // Require Sass file so webpack can build it
 import 'bootstrap/dist/css/bootstrap.css';
 import'./styles/style.css';
 
 const appConfig = new AppConfig()
-const { userSession } = ReactBlockstack(appConfig)
+const blockstack = ReactBlockstack(appConfig)
 
-ReactDOM.render(<Router><Blockstack><App/></Blockstack></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
