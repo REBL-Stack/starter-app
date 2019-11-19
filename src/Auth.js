d1008180b0fb004dd408305373ebb7facf220d1b
Terje Norderhaug <terje@in-progress.com>
2019-11-18 16:31:06 -0800

    Generalize authentication button

--- src/Auth.js
+++ src/Auth.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { useBlockstack } from 'react-blockstack'
 
 export default function Auth (props) {
-  const { signOut } = useBlockstack()
+  const { signIn, signOut } = useBlockstack()
   return (
     <button
       className="btn btn-primary btn-lg"
-      id="signout-button"
-      disabled={ !signOut }
-      onClick={ signOut }>
-      Logout
+      disabled={ !signIn && !signOut }
+      onClick={ signIn || signOut }>
+      {signIn ? "Signin" : signOut ? "Logout" : "..."}
     </button>
     )
 }
