1c2486ebdf2a6b9b1af6b73df70a4d3edef26fed
Terje Norderhaug <terje@in-progress.com>
2019-11-18 16:19:11 -0800

    Change component demonstrating BlockstackContext

--- src/Landing.js
+++ src/Landing.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { useBlockstack } from 'react-blockstack'
+import React, { Component } from 'react'
+import BlockstackContext from 'react-blockstack/dist/context'
 
-export default function Landing (props) {
-  const { signIn } = useBlockstack()
-  return (
-    <div className="panel-landing" id="section-1">
-      <h1 className="landing-heading">Hello, Blockstack!</h1>
-      <p className="lead">
-        <button
-          className="btn btn-primary btn-lg"
-          id="signin-button"
-          disabled={ !signIn }
-          onClick={ signIn }>
-          Sign In with Blockstack
-        </button>
-      </p>
-    </div>
-  )
+// Demonstrating Blockstack Context for compatibility with React Class Components
+
+export default class Landing extends Component {
+  static contextType = BlockstackContext
+  render () {
+    const { signIn } = this.context
+    return (
+      <div className="panel-landing" id="section-1">
+        <h1 className="landing-heading">Hello, Blockstack!</h1>
+        <p className="lead">
+          <button
+            className="btn btn-primary btn-lg"
+            id="signin-button"
+            disabled={ !signIn }
+            onClick={ signIn }>
+            Sign In with Blockstack
+          </button>
+        </p>
+      </div>
+    )
+  }
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import BlockstackContext from 'react-blockstack/dist/context'
+import React from 'react'
+import { useBlockstack} from 'react-blockstack'
 
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
@@ -15,22 +15,18 @@ function Auth ({signOut}) {
     )
 }
 
-export default class Profile extends Component {
-  render () {
-    const { person } = this.props
-    const { signOut } = this.context
-    return (
-      <div className="panel-welcome" id="section-2">
-        <div className="avatar-section">
-          <img src={ (person && person.avatarUrl()) || avatarFallbackImage }
-               className="img-rounded avatar" id="avatar-image" alt=""/>
-        </div>
-        <h1>Hello, <span id="heading-name">{ (person && person.name()) || 'Nameless Person' }</span>!</h1>
-        <p className="lead">
-          <Auth signOut={signOut} />
-        </p>
+export default function Profile ({ person }) {
+  const { signOut } = useBlockstack()
+  return (
+    <div className="panel-welcome" id="section-2">
+      <div className="avatar-section">
+        <img src={ (person && person.avatarUrl()) || avatarFallbackImage }
+             className="img-rounded avatar" id="avatar-image" alt="Avatar"/>
       </div>
-    )
-  }
+      <h1>Hello, <span id="heading-name">{ (person && person.name()) || 'Nameless Person' }</span>!</h1>
+      <p className="lead">
+        <Auth signOut={signOut} />
+      </p>
+    </div>
+  )
 }
-Profile.contextType = BlockstackContext
