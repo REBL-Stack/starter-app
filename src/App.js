37163079b93d6a74b4faa427019adc479b771ee7
Terje Norderhaug <terje@in-progress.com>
2019-12-07 18:11:29 -0800

    Create cards

--- src/App.js
+++ src/App.js
@@ -8,12 +8,8 @@ export default function App (props) {
   const { person, signIn } = useBlockstack()
   return (
   <Blockstack>
-    <div className="jumbotron jumbotron-fluid pt-3 mb-0">
-      <div className="container">
-         {signIn && <Landing/>}
-         {person && <Main person={person}/>}
-      </div>
-    </div>
+     {signIn && <Landing/>}
+     {person && <Main person={person}/>}
   </Blockstack>
   )
 }
