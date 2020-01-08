d1d6e22ae20df0470526ad2c06aaa4f59c226582
Friedger Müffke <mail@friedger.de>
2020-01-08 14:55:41 +0100

    add blockstack prettier config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { useBlockstack} from 'react-blockstack'
-import { Blockstack } from 'react-blockstack/dist/context'
+import { useBlockstack } from 'react-blockstack';
+import { Blockstack } from 'react-blockstack/dist/context';
 import Main from './Main.js';
 import Landing from './Landing.js';
 
-export default function App (props) {
-  const { person, signIn } = useBlockstack()
+export default function App(props) {
+  const { person, signIn } = useBlockstack();
   return (
-  <Blockstack>
-     {signIn && <Landing/>}
-     {person && <Main person={person}/>}
-  </Blockstack>
-  )
+    <Blockstack>
+      {signIn && <Landing />}
+      {person && <Main person={person} />}
+    </Blockstack>
+  );
 }
