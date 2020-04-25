aefa14638492b5241bc0d16c37409f9b9d1da677
Terje Norderhaug <terje@in-progress.com>
2020-04-25 00:56:44 -0700

    Qualify icon url

--- src/Auth.js
+++ src/Auth.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useBlockstack } from 'react-blockstack';
 
+// Authentication button adapting to status
+
 export default function Auth(props) {
   const { signIn, signOut } = useBlockstack();
   return (
