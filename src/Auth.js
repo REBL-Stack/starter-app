f9ff3aad3f5b75fd884b3a87c811baa6e691f0c8
Friedger Müffke <mail@friedger.de>
2020-04-27 17:32:01 +0200

    use connect for auth button

--- src/Auth.js
+++ src/Auth.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { useBlockstack } from 'react-blockstack';
+import React, { useCallback } from 'react';
+import { useBlockstack, useConnectOptions } from 'react-blockstack';
+import { connectOptions } from './UserSession';
+import { showBlockstackConnect } from '@blockstack/connect/dist/connect.cjs.production.min';
 
 // Authentication button adapting to status
 
 export default function Auth(props) {
-  const { signIn, signOut } = useBlockstack();
+  const { signIn, signOut, userSession } = useBlockstack();
+  const authOptions = useConnectOptions(connectOptions(userSession));
+  const buttonAction = useCallback(() => {
+    signIn ? showBlockstackConnect(authOptions) : signOut();
+  }, [signIn, authOptions, signOut]);
+
   return (
     <button
       className="btn btn-primary btn-lg"
       disabled={!signIn && !signOut}
-      onClick={signIn || signOut}
+      onClick={buttonAction}
     >
       {signIn ? 'Sign In' : signOut ? 'Log Out' : '...'}
     </button>
